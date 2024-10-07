10e31928b9e0733066a88c7c362fd03b17a7e9a1
josip-krnic <krnic.josip.jk@gmail.com>
2024-10-07 19:48:19 +0200

    Prva vjezba

--- Vjezba 1/Vjezba.cpp
+++ Vjezba 1/Vjezba.cpp
@@ -1,4 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define MAX 1024
+
+int NumOfRows(const char* filename) {
+	FILE* dat = NULL;
+	dat = fopen(filename, "r");
+	int NumOfRows = 0;
+	int n;
+	char Buffer[MAX];
+	if (!fp) return -1;
+	while (1) {
+		fgets(Buffer, n, dat);
+		NumOfRows++;
+		if (feof(dat) != 0) break;
+
+	}
+
+}
+
 
 int main() {
 	printf("Hello World!");
