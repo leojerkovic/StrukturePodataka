dcd14ab2159a34936dbb4e53ff58b4e7fa6e1a1d
leojerkovic <leo.jerkovic10@gmail.com>
2024-10-16 20:40:31 +0200

    Vjezba 2 poboljsana

--- Vjezba 2/Vjezba2Doma.c
+++ Vjezba 2/Vjezba2Doma.c
@@ -14,7 +14,7 @@ struct osoba* Create(char*, char*, int);
 struct osoba* ReturnFirst(struct osoba**, char*, char*, int);
 int ListOut(struct osoba*);
 struct osoba* ReturnLast(struct osoba*);
-int AddLast(struct osoba*, char*, char*, int);
+int AddLast(struct osoba**, char*, char*, int);
 struct osoba* ReturnSurname(struct osoba*, char*);
 int DeletePerson(struct osoba**, char*);
 
@@ -24,37 +24,31 @@ int main() {
 	//testiranje funkcija s primjerima
 
 	struct osoba* head = NULL;
-	char ime1[15] = { "Joe" }, prezime1[15] = { "Anderson" }; int godrod1 = 1950;
+	char ime1[15] = { "Walter" }, prezime1[15] = { "White" }; int godrod1 = 1958;
 	char ime2[15] = { "Alan" }, prezime2[15] = { "Wake" }; int godrod2 = 1977;
 	char ime3[15] = { "Dexter" }, prezime3[15] = { "Morgan" }; int godrod3 = 1970;
 	char ime4[15] = { "Kazuma" }, prezime4[15] = { "Kiryu" }; int godrod4 = 1968;
-	struct osoba* prva = ReturnFirst(&head, ime1, prezime1, godrod1);
+	char ime5[15] = { "Isshin" }, prezime5[15] = { "Ashina" }; int godrod5 = 1938;
+	struct osoba* prva = ReturnFirst(&head, ime1, prezime1, godrod1); //Tri elementa dodana na prvo mjesto (s pridruzivanjem)
 	struct osoba* druga = ReturnFirst(&head, ime2, prezime2, godrod2);
 	struct osoba* cetvrta = ReturnFirst(&head, ime4, prezime4, godrod4);
-	if (ListOut(head)) {
-		printf("ERROR LISTING");
-	}
-	if (AddLast(head, ime3, prezime3, godrod3)) {
-		printf("ERROR ADDING LAST");
-	}
-	else {
-		printf("\nAdded person to the end of list: %s %s, born: %d.\n", ime3, prezime3,godrod3);
-	}
-	if (ListOut(head)) {
-		printf("ERROR LISTING");
-	}
+
+	ListOut(head); //Ispis elemenata
+		
+	if(!AddLast(&head, ime3, prezime3, godrod3)) //Dodavanje elementa na zadnje mjesto (bez pridruzivanja)
+	printf("\nAdded person to the end of list: %s %s, born: %d.\n", ime3, prezime3,godrod3);
+	
+	ListOut(head); //Ispis elemenata
+
 	struct osoba* temp = NULL;
-	temp = ReturnSurname(head, prezime2);
+	temp = ReturnSurname(head,prezime2); //Pronalazak po prezimenu i pridruzivanje
+	if(temp!=NULL)
 	printf("\nReturned with surname '%s': %s %s, born: %d.\n", prezime2, temp->ime, temp->prezime,temp->godrod);
-	if (DeletePerson(&head, prezime1)) {
-		printf("ERROR DELETING PERSON");
-	}
-	else {
-		printf("\nDeleted from list: %s %s, born: %d.\n", ime1, prezime1, godrod1);
-	}
-	if (ListOut(head)) {
-		printf("ERROR LISTING");
-	}
+
+	if (!DeletePerson(&head, prezime1)) //Brisanje elementa po prezimenu
+	printf("\nDeleted from list: %s %s, born: %d.\n", ime1, prezime1, godrod1);
+	
+	ListOut(head); //Ispis elemenata
 
 	return 0;
 }
@@ -65,7 +59,7 @@ struct osoba* Create(char* ime, char* prezime, int godrod) {
 	struct osoba* Novi = NULL;
 	Novi = (struct osoba*)malloc(sizeof(struct osoba));
 	if (Novi == NULL) {
-		printf("Error allocating memory");
+		printf("\nError allocating memory\n");
 		return NULL;
 	}
 	strcpy(Novi->ime, ime);
@@ -78,7 +72,7 @@ struct osoba* ReturnFirst(struct osoba** head, char* ime, char* prezime, int god
 	struct osoba* Novi = NULL;
 	Novi = Create(ime, prezime, godrod);
 	if (Novi == NULL) {
-		printf("Error allocating memory");
+		printf("\nError allocating memory\n");
 		return NULL;
 	}
 	Novi->next = (*head);
@@ -90,7 +84,7 @@ struct osoba* ReturnFirst(struct osoba** head, char* ime, char* prezime, int god
 int ListOut(struct osoba* head) {
 	struct osoba* temp=head;
 	if (temp == NULL) {
-		printf("No elements to print\n");
+		printf("\nNo elements to print\n");
 		return EXIT_FAILURE;
 	}
 	printf("\nList:\n");
@@ -105,6 +99,7 @@ int ListOut(struct osoba* head) {
 struct osoba* ReturnLast(struct osoba* head) {
 	struct osoba* temp = head;
 	if (temp == NULL) {
+		printf("\nHead is the only element\n");
 		return temp;
 	}
 	while (1) {
@@ -113,16 +108,16 @@ struct osoba* ReturnLast(struct osoba* head) {
 	}
 }
 
-int AddLast(struct osoba* head, char* ime, char* prezime, int godrod) {
-	struct osoba* beforelast = ReturnLast(head);
+int AddLast(struct osoba** head, char* ime, char* prezime, int godrod) {
+	struct osoba* beforelast = ReturnLast((*head));
 	struct osoba* last = Create(ime, prezime, godrod);
 	if (last == NULL) {
-		printf("Error allocating memory");
+		printf("\nError allocating memory\n");
 		return EXIT_FAILURE;
 	}
 	last->next=NULL;
 	if (beforelast == NULL) {
-		beforelast = last;
+		(*head) = last;
 	}
 	else {
 		beforelast->next = last;
@@ -133,7 +128,7 @@ int AddLast(struct osoba* head, char* ime, char* prezime, int godrod) {
 struct osoba* ReturnSurname(struct osoba* head, char* prezime) {
 	struct osoba* temp = head;
 	if (temp == NULL) {
-		printf("No elements to return");
+		printf("\nNo elements to return\n");
 		return NULL;
 	}
 	while (1) {
@@ -141,7 +136,7 @@ struct osoba* ReturnSurname(struct osoba* head, char* prezime) {
 			return temp;
 		}
 		else if (temp->next == NULL) {
-			printf("Person not found");
+			printf("\nPerson not found\n");
 			return NULL;
 		}
 		temp = temp->next;
@@ -152,14 +147,19 @@ struct osoba* ReturnSurname(struct osoba* head, char* prezime) {
 int DeletePerson(struct osoba** head, char* prezime) {
 	struct osoba* temp=(*head);
 	if (temp == NULL) {
-		printf("No elements to delete");
+		printf("\nNo elements to delete\n");
 		return EXIT_FAILURE;
 	}
 	struct osoba* toDelete = NULL;
 	toDelete = ReturnSurname((*head), prezime);
+	if (toDelete == NULL) {
+		printf("\nError deleting person\n");
+		return EXIT_FAILURE;
+	}
 	if (toDelete == (*head)) {
 		free(toDelete);
 		(*head) = NULL;
+		return EXIT_SUCCESS;
 	}
 	while (1) {
 		if (temp->next == toDelete) {
@@ -169,7 +169,7 @@ int DeletePerson(struct osoba** head, char* prezime) {
 		}
 		temp = temp->next;
 		if (temp->next == NULL) {
-			printf("Person with surname not found");
+			printf("\nPerson with surname not found\n");
 			return EXIT_FAILURE;
 		}
 	}
